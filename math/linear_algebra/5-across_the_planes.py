1c1074e3766aa5ead55e432d00be2100cbc85882
reha96 <reha.tuncer@gmail.com>
2026-05-12 20:53:34 +0200

    Update 5-across_the_planes.py

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -25,6 +25,8 @@ matrices element-wise
     """
     if len(mat1) != len(mat2):
         return None
+    if len(mat1[0]) != len(mat2[0]):
+        return None
     out = [[0] * len(mat1[0]) for _ in range(len(mat1))]
     for i in range(len(mat1)):
         for j in range(len(mat1[i])):
