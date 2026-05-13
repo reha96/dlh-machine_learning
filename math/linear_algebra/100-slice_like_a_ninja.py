4dfc31e372f42f8328b8aabb5c90411b806ec351
reha96 <reha.tuncer@gmail.com>
2026-05-13 18:25:36 +0200

    Create 100-slice_like_a_ninja.py

---
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Write a function def np_slice(matrix, axes={}): that
+slices a matrix along specific axes:
+
+    You can assume that matrix is a numpy.ndarray
+    You must return a new numpy.ndarray
+    axes is a dictionary where the key is an axis to slice
+    along and the value is a tuple representing the slice
+    to make along that axis
+    You can assume that axes represents a valid slice
+    """
+
+
+import numpy as np
+
+
+def np_slice(matrix, axes={}):
+    items = list(axes.items())
+    print(items[0], items[0][0], items[0][1])
+    # out = matrix[items[0], items[1][0]:items[1][1]]
+    # return out
+
+
+mat1 = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print(np_slice(mat1, axes={1: (1, 3)}))
+# print(mat1)
+# mat2 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+#                  [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]],
+#                  [[21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]])
+# print(np_slice(mat2, axes={0: (2,), 2: (None, None, -2)}))
+# print(mat2)
+
