97f297b93d1c6959843f272112f66ef1e64d4286
reha96 <reha.tuncer@gmail.com>
2026-05-13 10:30:57 +0200

    Update 3-flip_me_over.py

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -27,12 +27,3 @@ def matrix_transpose(matrix):
             print(temp)
         out.append(temp)
     return out
-
-
-mat1 = [[1, 2], [3, 4]]
-# print(mat1)
-print(matrix_transpose(mat1))
-# mat2 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15],
-#         [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]
-# print(mat2)
-# print(matrix_transpose(mat2))
