74a5b40510423b39a40d1add833efb071c53d123
reha96 <reha.tuncer@gmail.com>
2026-05-13 14:57:04 +0200

    Create 12-bracin_the_elements.py

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Write a function def np_elementwise(mat1, mat2): that performs element-wise addition, subtraction, multiplication, and division:
+
+    You can assume that mat1 and mat2 can be interpreted as numpy.ndarrays
+    You should return a tuple containing the element-wise sum, difference, product, and quotient, respectively
+    You are not allowed to use any loops or conditional statements
+    You can assume that mat1 and mat2 are never empty
+
+    """
+
+
+def np_elementwise(mat1, mat2):
+    """function def np_elementwise(mat1, mat2): that performs element-wise addition, subtraction, multiplication, and
+    division:
+
+    Args:
+        mat1 (_type_): _description_
+        mat2 (_type_): _description_
+    """
+    return (np.add(mat1, mat2), np.subtract(mat1, mat2),
+            np.multiply(mat1, mat2), np.divide(mat1, mat2))
