661bada18b073ec2175043d8bb6773a16e57c869
reha96 <reha.tuncer@gmail.com>
2026-05-13 12:55:52 +0200

    Create 10-ill_use_my_scale.py

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Write a function def np_shape(matrix): that calculates the shape of a numpy.ndarray:
+
+    You are not allowed to use any loops or conditional statements
+    You are not allowed to use try/except statements
+    The shape should be returned as a tuple of integers
+
+    """
+import numpy as np
+
+
+def np_shape(matrix):
+    """function def np_shape(matrix): that calculates the shape of a numpy.ndarray
+
+    Args:
+        matrix (_type_): _description_
+    """
+    return np.shape(matrix)
