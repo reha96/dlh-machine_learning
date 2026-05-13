1a4a6800c4b32dd7f23fe5a91c785b61c30913ce
reha96 <reha.tuncer@gmail.com>
2026-05-13 15:15:47 +0200

    Update 13-cats_got_your_tongue.py

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,16 +1,3 @@
-"""Write a function def np_cat(mat1, mat2, axis=0)
-that concatenates two matrices along a specific axis:
-You can assume that mat1 and mat2 can be interpreted
-as numpy.ndarrays
-You must return a new numpy.ndarray
-You are not allowed to use any loops or conditional
-statements
-You may use: import numpy as np
-You can assume that mat1 and mat2 are never empty
-
-    Returns:
-        _type_: _description_
-    """
 #!/usr/bin/env python3
 import numpy as np
 
