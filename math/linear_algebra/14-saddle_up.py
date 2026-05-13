4b2591471e7891e7d877f039bbbd295191f4a8eb
reha96 <reha.tuncer@gmail.com>
2026-05-13 15:20:06 +0200

    Create 14-saddle_up.py

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Write a function def np_matmul(mat1, mat2):
+that performs matrix multiplication:
+
+    You can assume that mat1 and mat2 are
+    numpy.ndarrays
+    You are not allowed to use any loops
+    or conditional statements
+    You may use: import numpy as np
+    You can assume that mat1 and mat2 are never empty
+
+
+    """
+
+
+def np_matmul(mat1, mat2):
+    """function def np_matmul(mat1, mat2):
+    that performs matrix multiplication:
+
+    Args:
+        mat1 (_type_): _description_
+        mat2 (_type_): _description_
+    """
+
+    return mat1@mat2
