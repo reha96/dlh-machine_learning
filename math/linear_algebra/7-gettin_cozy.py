53e7de35b1fdd5b0747fb1759b788eb778e78bd0
reha96 <reha.tuncer@gmail.com>
2026-05-14 11:26:56 +0200

    Update 7-gettin_cozy.py

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -20,29 +20,17 @@ def cat_matrices2D(mat1, mat2, axis=0):
         mat2 (_type_): _description_
         axis (int, optional): _description_. Defaults to 0.
     """
-    
-    try:
-        if axis == 0:
-            out = []
-            out.extend([mat1, mat2])
-        if axis == 1:
-            # rows =
-            # columns =
-            # out = [[0] * len(mat1[0]) for _ in range(len(mat1))]
-            # for i in range(len(mat1)):
-            #         out[i][j] = (mat1[i][j] + mat2[i][j])
-    except Exception:
+
+    if len(mat1) != len(mat2[0]) or axis > 1:
         return None
-    return out
+    out = []
+    if axis == 0:
+        out = mat1 + mat2
+        return out
 
-
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6]]
-mat3 = [[7], [8]]
-mat4 = cat_matrices2D(mat1, mat2)
-mat5 = cat_matrices2D(mat1, mat3, axis=1)
-print(mat4)
-print(mat5)
-[[1, 2], [3, 4], [5, 6]]
-[[1, 2, 5], [3, 4, 6]]
-# [[1, 2, 7], [3, 4, 8]]
+    if axis == 1:
+        for i in range(len(mat1)):
+            for j in range(len(mat2[i])):
+                mat1[i].append(mat2[i][j])
+        out.append(mat1)
+        return out
