cfe0058c896539c15b76d58bb48abcc27f464c83
reha96 <reha.tuncer@gmail.com>
2026-05-14 11:49:06 +0200

    Update 7-gettin_cozy.py

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -28,7 +28,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
     if len(mat1) == 0 or len(mat2) == 0:
         return None
     if axis == 0:
-        if len(mat1[0]) == len(mat2[0]):
+        if len(mat1[0]) != len(mat2[0]):
             return None
         out = mat1 + mat2
         return out
