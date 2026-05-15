916fef3766e1784e1cdf81da6c2318713f48a99f
reha96 <reha.tuncer@gmail.com>
2026-05-15 16:18:47 +0200

    Update 3-insert_value.sql

--- pipeline/databases/3-insert_value.sql
+++ pipeline/databases/3-insert_value.sql
@@ -1,3 +1,3 @@
 -- Write a script that inserts a new row in the table first_table in your MySQL server.
-INSERT INTO first_table (id, name,)
-values (89, "Holberton School");
+INSERT INTO first_table (id, name)
+values (89, Holberton School);
