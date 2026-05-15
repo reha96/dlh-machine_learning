53be252e669b87d31e6b8e1a29b7005e6cc02912
reha96 <reha.tuncer@gmail.com>
2026-05-15 16:49:19 +0200

    Update 6-avg_temperatures.sql

--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -1,4 +1,6 @@
 -- Write a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
 SELECT city,
     AVG(temp) as avg_temp
-FROM hbtn_0c_0 ORDER BY avg_temp DESC;
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
