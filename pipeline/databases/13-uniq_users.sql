e47a02e3969594ac3108fd9c6686af4eeebacba8
reha96 <reha.tuncer@gmail.com>
2026-05-16 23:30:14 +0200

    Create 13-uniq_users.sql

---
+++ pipeline/databases/13-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Write a SQL script that creates a table users following these requirements:
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(256) NOT NULL UNIQUE,
+    name VARCHAR(256),
+    PRIMARY KEY (id)
+);
