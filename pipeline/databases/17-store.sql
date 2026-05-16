dcc2d5028a2e37318157021dc5258416739b871b
reha96 <reha.tuncer@gmail.com>
2026-05-17 00:33:57 +0200

    Update 17-store.sql

--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -1,4 +1,6 @@
 -- Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
+DELIMITER $$
+
 CREATE TRIGGER decrease_quantity_after_order
 AFTER INSERT ON orders
 FOR EACH ROW
@@ -6,4 +8,6 @@ BEGIN
     UPDATE items
     SET quantity = quantity - NEW.number
     WHERE id = NEW.item_id;
-END
+END$$
+
+DELIMITER;
