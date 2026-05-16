7a2a43a848d5c0a465e953cb2ef8e9828a0d085e
reha96 <reha.tuncer@gmail.com>
2026-05-17 00:00:40 +0200

    Create 16-glam_rock.sql

---
+++ pipeline/databases/16-glam_rock.sql
@@ -0,0 +1,6 @@
+-- Write a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, (2020 - formed) AS life_span
+FROM metal_bands
+WHERE
+    split IS NULL
+ORDER BY life_span DESC
