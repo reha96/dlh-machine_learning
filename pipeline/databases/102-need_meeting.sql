d272ed16e10981c950c95fe8dce42f1d8515330f
reha96 <reha.tuncer@gmail.com>
2026-05-25 20:47:18 +0200

    Update 102-need_meeting.sql

--- pipeline/databases/102-need_meeting.sql
+++ pipeline/databases/102-need_meeting.sql
@@ -3,7 +3,7 @@ CREATE VIEW need_meeting AS
 SELECT *
 FROM students
 WHERE
-    score > 80
+    score < 80
     AND (
         last_meeting IS NULL
         OR last_meeting > NOW() - INTERVAL 1 MONTH
