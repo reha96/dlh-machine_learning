9bdf19de614f19383c92496764f89931e3f63c24
reha96 <reha.tuncer@gmail.com>
2026-05-25 20:49:10 +0200

    Update 102-need_meeting.sql

--- pipeline/databases/102-need_meeting.sql
+++ pipeline/databases/102-need_meeting.sql
@@ -1,6 +1,6 @@
 -- Write a SQL script that creates a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month.
 CREATE VIEW need_meeting AS
-SELECT *
+SELECT name
 FROM students
 WHERE
     score < 80
