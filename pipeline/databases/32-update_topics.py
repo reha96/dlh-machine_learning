20bc9100cb61357e6d336824a773868cb99b37fa
reha96 <reha.tuncer@gmail.com>
2026-05-25 16:23:05 +0200

    Create 32-update_topics.py

---
+++ pipeline/databases/32-update_topics.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Write a Python function that changes all topics
+    of a school document based on the name:
+    """
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics
+    of a school document
+
+    Args:
+        mongo_collection (_type_): _description_
+        name (_type_): _description_
+        topics (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return mongo_collection.update(name, topics)
