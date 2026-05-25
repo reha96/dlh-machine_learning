3e3bda343a5dc23d8b0a0069ca921d92e01004dc
reha96 <reha.tuncer@gmail.com>
2026-05-25 20:03:28 +0200

    Update 34-log_stats.py

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     print("Methods")
     for method in methods:
         count = collection.count_documents({'method': method})
-        print(f"    method {method}: {count}")
+        print(f"\tmethod {method}: {count}")
     status = collection.count_documents({'method': "GET", 'path': "/status"})
     print(f"{status} status check")
