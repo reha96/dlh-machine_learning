1f6e6842954c24b9aee8e875956a92ef6d9b3364
reha96 <reha.tuncer@gmail.com>
2026-05-25 21:09:50 +0200

    Update 103-average_weighted_score.sql

--- pipeline/databases/103-average_weighted_score.sql
+++ pipeline/databases/103-average_weighted_score.sql
@@ -11,7 +11,7 @@ BEGIN
     DECLARE v_weighted_average_score FLOAT;
 
     -- select and calculate student weighted average score
-    SELECT AVG(score)*projects.weight INTO v_weighted_average_score
+    SELECT SUM(score*projects.weight)/COUNT(score) INTO v_weighted_average_score
     FROM corrections
     JOIN projects ON corrections.project_id = projects.id
     WHERE user_id = p_user_id;
