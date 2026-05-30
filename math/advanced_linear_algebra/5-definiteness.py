733d31f243ffe89c240833fac5d7668533665a37
reha96 <reha.tuncer@gmail.com>
2026-05-30 13:24:20 +0200

    Update 5-definiteness.py

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -6,6 +6,22 @@ import numpy as np
 
 
 def definiteness(matrix):
+    """calculates the definiteness of a matrix
+    using eigenvals from numpy
+
+    Args:
+        matrix (_type_): _description_
+
+    Raises:
+        TypeError: _description_
+
+    Returns:
+        _type_: _description_
+    """
+    trans = np.transpose(matrix)
+
+    if trans != matrix:
+        return None
 
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
