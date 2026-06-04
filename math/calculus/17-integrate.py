14c76811e96a09277fe5183367e3564ab346aac7
reha96 <reha.tuncer@gmail.com>
2026-06-04 09:01:46 +0200

    Create 17-integrate.py

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Write a function def poly_integral(poly, C=0):
+    that calculates the integral of a polynomial: 
+    """
+
+
+def poly_integral(poly, C=0):
+    """calculates the integral of a polynomial
+
+    Args:
+        poly (_type_): _description_
+        C (int, optional): _description_. Defaults to 0.
+    """
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+    out = [0]
+    for i in range(len(poly)):
+        out.append(poly[i]/(i+1))
+    return out
