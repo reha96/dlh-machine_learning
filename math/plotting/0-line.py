a1f9151b6dad2628283858b300e34c1e90f2c034
reha96 <reha.tuncer@gmail.com>
2026-06-05 12:38:50 +0200

    Update 0-line.py

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -16,6 +16,6 @@ def line():
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.plot(y, color='r') # set color
-    plt.xlim(0, 10) # set x limit
-    return plt.show() # return printed
+    plt.plot(y, color='r')  # set color
+    plt.xlim(0, 10)  # set x limit
+    return plt.show()  # return printed
