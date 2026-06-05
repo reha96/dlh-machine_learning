5c049f25f0ecfb4846d2f4bdb948ec755e4a6636
reha96 <reha.tuncer@gmail.com>
2026-06-05 12:39:12 +0200

    Update 0-line.py

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import numpy as np
-import matplotlib.pyplot as plt
-
 """create simple line plot, line col red
     """
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def line():
