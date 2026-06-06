fb6878a574407a8ea71200ffc710a5b9436bc8dc
reha96 <reha.tuncer@gmail.com>
2026-06-06 18:47:36 +0200

    Update 4-frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -14,12 +14,16 @@ import matplotlib.pyplot as plt
 
 
 def frequency():
+    """create histogram with specific nb. of bins
+
+    Returns:
+        _type_: _description_
+    """
 
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
     bins = np.arange(0, 101, 10)
-    yticks = np.arange(0, 31, 5)
     plt.hist(
         student_grades,
         bins=bins,
@@ -29,6 +33,7 @@ def frequency():
     plt.ylabel("Number of Students")
     plt.title("Project A")
     plt.xticks(bins)
-    plt.yticks(yticks)
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
 
     return plt.show()
