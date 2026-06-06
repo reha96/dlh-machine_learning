5efff8e8a53a54fb79f4aeaf3558344a0868b438
reha96 <reha.tuncer@gmail.com>
2026-06-06 18:18:30 +0200

    Update 3-two.py

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
-"""Complete the following source code to plot x ↦ y1 and x ↦ y2 as line graphs:
+"""Complete the following source code to plot x ↦ y1
+and x ↦ y2 as line graphs:
 
 The x-axis should be labeled Time (years)
 The y-axis should be labeled Fraction Remaining
-The title should be Exponential Decay of Radioactive Elements
+The title should be Exponential Decay of Radioactive
+Elements
 The x-axis should range from 0 to 20,000
 The y-axis should range from 0 to 1
 x ↦ y1 should be plotted with a dashed red line
 x ↦ y2 should be plotted with a solid green line
-A legend labeling x ↦ y1 as C-14 and x ↦ y2 as Ra-226 should be placed in the upper right hand corner of the plot
+A legend labeling x ↦ y1 as C-14 and x ↦ y2 as Ra-226
+should be placed in the upper right hand corner of the plot
     """
 import numpy as np
 import matplotlib.pyplot as plt
