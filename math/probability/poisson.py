7174ef69f8660b894825ace32f7d34f219bf20f2
reha96 <reha.tuncer@gmail.com>
2026-06-10 18:22:22 +0200

    cr 0 and readme

---
+++ math/probability/poisson.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Create a class Poisson that represents a poisson distribution:
+
+Class contructor def __init__(self, data=None, lambtha=1.):
+data is a list of the data to be used to estimate the distribution
+lambtha is the expected number of occurences in a given time frame
+Sets the instance attribute lambtha
+Saves lambtha as a float
+If data is not given, (i.e. None (be careful: not data
+has not the same result as data is None)):
+Use the given lambtha
+If lambtha is not a positive value or equals to 0, raise
+a ValueError with the message lambtha must be a positive value
+If data is given:
+Calculate the lambtha of data
+If data is not a list, raise a TypeError with the message
+data must be a list
+If data does not contain at least two data points,
+raise a ValueError with the message data must contain multiple values 
+
+"""
+
+
+class Poisson:
+    """class that represents a poisson distribution
+    """
+
+    def __init__(self, data=None, lambtha=1.):
+        """class contructor
+
+        Args:
+            data (_type_, optional): _description_. Defaults to None.
+            lambtha (_type_, optional): _description_. Defaults to 1..
+        """
+        self.lambtha = float(lambtha)
+        if self.lambtha <= 0:
+            raise ValueError("lambtha must be a positive value")
+        if data is None:
+            return self.lambtha
+        elif not isinstance(data, list):
+            raise TypeError("data must be a list")
+        elif len(data) < 2:
+            raise ValueError("data must contain multiple values")
+
+        sum = 0
+        for i in range(len(data)):
+            sum += data[i]
+        self.lambtha = sum/len(data)
+        return self.lambtha
