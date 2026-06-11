c9853a2b0f942e2c728e922834bf27787174aac2
reha96 <reha.tuncer@gmail.com>
2026-06-11 11:47:00 +0200

    Create exponential.py

---
+++ math/probability/exponential.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Create a class Exponential that represents an exponential
+distribution:
+
+Class contructor def __init__(self, data=None, lambtha=1.):
+data is a list of the data to be used to estimate the distribution
+lambtha is the expected number of occurences in a given time frame
+Sets the instance attribute lambtha
+Saves lambtha as a float
+If data is not given (i.e. None):
+Use the given lambtha
+If lambtha is not a positive value, raise a ValueError
+with the message lambtha must be a positive value
+If data is given:
+Calculate the lambtha of data
+If data is not a list, raise a TypeError with the message data must
+be a list
+If data does not contain at least two data points, raise a
+ValueError with the message data must contain multiple values
+    """
+
+
+class Exponential:
+    """class that represents an exponential
+distribution
+    """
+
+    def __init__(self, data=None, lambtha=1.):
+        """class constructor
+
+        Args:
+            data (_type_, optional): _description_. Defaults to None.
+            lambtha (_type_, optional): _description_. Defaults to 1..
+        """
+        if self.lambtha <= 0:
+            raise ValueError("lambtha must be a positive value")
+        if data is None:
+            return None
+        elif not isinstance(data, list):
+            raise TypeError("data must be a list")
+        elif len(data) < 2:
+            raise ValueError("data must contain multiple values")
+
+        sum = 0
+        for i in range(len(data)):
+            sum += data[i]
+        self.lambtha = 1/(sum/len(data))
+        return None
