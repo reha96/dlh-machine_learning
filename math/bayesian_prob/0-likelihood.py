680a5c528b6fda586bf070888213e08ce698ea63
reha96 <reha.tuncer@gmail.com>
2026-06-15 15:17:26 +0200

    Update 0-likelihood.py

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -39,13 +39,6 @@ likelihood of obtaining data
         n (_type_): _description_
         P (_type_): _description_
     """
-    if not isinstance(n, int) or n <= 0:
-        raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than\
-            or equal to 0")
-    if x > n:
-        raise ValueError("x cannot be greater than n")
     if not isinstance(P, numpy.ndarray) or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
 
@@ -53,6 +46,13 @@ likelihood of obtaining data
         if not (0 <= P[i] <= 1):
             raise ValueError("All values in\
                 P must be in the range [0, 1]")
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError("x must be an integer that is greater than\
+            or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
 
     a = 1.0
     for i in range(1, x + 1):
