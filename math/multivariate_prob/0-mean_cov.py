fb6634420e1cb9793e249ae9b0bd8f8dae04ba19
reha96 <reha.tuncer@gmail.com>
2026-06-25 10:18:37 +0200

    Create 0-mean_cov.py

---
+++ math/multivariate_prob/0-mean_cov.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Write a function def mean_cov(X): that calculates
+the mean and covariance of a data set:
+
+X is a numpy.ndarray of shape (n, d) containing the data set:
+n is the number of data points
+d is the number of dimensions in each data point
+If X is not a 2D numpy.ndarray, raise a TypeError with the message
+X must be a 2D numpy.ndarray
+If n is less than 2, raise a ValueError with the message
+X must contain multiple data points
+Returns: mean, cov:
+mean is a numpy.ndarray of shape (1, d)
+containing the mean of the data set
+cov is a numpy.ndarray of shape (d, d)
+containing the covariance matrix of the data set
+You are not allowed to use the function numpy.cov
+
+"""
+
+
+import numpy as np
+
+
+def mean_cov(X):
+    """calculates the mean and covariance of a data set
+
+    Args:
+        X (_type_): _description_
+    """
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    n, d = X.shape
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+    mean = np.mean(X, axis=0)
+    inner_1 = X - mean
+    inner_2 = np.transpose(inner_1)
+    cov = np.mean(inner_2@inner_1, 0)/n
+    return mean, cov
