e853a1a94d4f10c4fbae43107563e023ebe341f8
reha96 <reha.tuncer@gmail.com>
2026-06-25 08:46:50 +0200

    Create 1-correlation.py

---
+++ math/multivariate_prob/1-correlation.py
@@ -0,0 +1,31 @@
+"""Write a function def correlation(C): that calculates
+a correlation matrix:
+
+C is a numpy.ndarray of shape (d, d) containing a covariance matrix
+d is the number of dimensions
+If C is not a numpy.ndarray, raise a TypeError with the message
+C must be a numpy.ndarray
+If C does not have shape (d, d), raise a ValueError with the message
+C must be a 2D square matrix
+Returns a numpy.ndarray of shape (d, d) containing
+the correlation matrix
+
+"""
+import numpy as np
+
+
+def correlation(C):
+    """calculates a correlation matrix
+
+    Args:
+        C (_type_): _description_
+    """
+    if not isinstance(C, np.ndarray):
+        raise TypeError("C must be a numpy.ndarray")
+    if C.ndim != 2 or C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
+
+    diagonal = np.diagonal(C)
+    outer = np.diagflat(diagonal)**(-1/2)
+    Co = outer @ C @ outer
+    return Co
