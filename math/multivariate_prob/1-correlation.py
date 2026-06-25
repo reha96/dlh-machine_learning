5972acb7be5d90ee5292b239a64bd132850653d3
reha96 <reha.tuncer@gmail.com>
2026-06-25 08:49:48 +0200

    Update 1-correlation.py

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python3
-"""Write a function def correlation(C): that calculates
-a correlation matrix:
-
-C is a numpy.ndarray of shape (d, d) containing a covariance matrix
-d is the number of dimensions
-If C is not a numpy.ndarray, raise a TypeError with the message
-C must be a numpy.ndarray
-If C does not have shape (d, d), raise a ValueError with the message
-C must be a 2D square matrix
-Returns a numpy.ndarray of shape (d, d) containing
-the correlation matrix
+"""Write a function that calculates
+a correlation matrix
 
 """
 import numpy as np
