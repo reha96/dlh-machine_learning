48c9d3e3a94c44d5c2a4d4cbd52b517ce7244930
reha96 <reha.tuncer@gmail.com>
2026-06-25 11:31:47 +0200

    Update 1-correlation.py

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
-"""Write a function that calculates
-a correlation matrix
+"""Write a function def correlation(C): that calculates
+a correlation matrix:
+
+C is a numpy.ndarray of shape (d, d) containing a covariance matrix
+d is the number of dimensions
+If C is not a numpy.ndarray, raise a TypeError with the message
+C must be a numpy.ndarray
+If C does not have shape (d, d), raise a ValueError with the message
+C must be a 2D square matrix
+Returns a numpy.ndarray of shape (d, d) containing
+the correlation matrix
 
 """
 import numpy as np
@@ -17,7 +26,8 @@ def correlation(C):
     if C.ndim != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
 
-    diagonal = np.diagonal(C)
-    outer = np.diagflat(diagonal)**(-1/2)
-    Co = outer @ C @ outer
+    diag = np.diagonal(C)  # extract diagonal values in 1D
+    outer = diag**(-1/2)  # apply power transform
+    outer_diag = np.diagflat(outer)  # build actual diagonal matrix
+    Co = outer_diag @ C @ outer_diag
     return Co
