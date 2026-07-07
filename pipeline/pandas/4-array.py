8a064d397f9fe99dee7a1c3f4af3576103563f88
reha96 <reha.tuncer@gmail.com>
2026-07-07 22:24:48 +0200

    Update 4-array.py

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -6,11 +6,10 @@ df is a pd.DataFrame containing columns named
 High and Close.
 The function should select the last 10 rows of the
 High and Close columns.
-Convert these selected values into a numpy.ndarray.
-Returns: the numpy.ndarray
+Convert these selected values into a numpy ndarray.
+Returns: the numpy ndarray
 
 """
-import pandas as pd
 
 
 def array(df):
