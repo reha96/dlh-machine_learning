38601e7249ba6170757e191ec1ec81845d53fd70
reha96 <reha.tuncer@gmail.com>
2026-07-08 18:36:43 +0200

    Create 9-fill.py

---
+++ pipeline/pandas/9-fill.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Write a function def fill(df): that takes a pd.DataFrame and:
+
+Removes the Weighted_Price column.
+Fills missing values in the Close column with the previous row’s value.
+Fills missing values in the High, Low, and Open columns with
+the corresponding Close value in the same row.
+Sets missing values in Volume_(BTC) and Volume_(Currency) to 0.
+Returns: the modified pd.DataFrame.
+
+"""
+
+
+def fill(df):
+    """Write a function def fill(df): that takes a pd.DataFrame and:
+
+    Removes the Weighted_Price column.
+    Fills missing values in the Close column with the previous
+    row’s value.
+    Fills missing values in the High, Low, and Open columns with
+    the corresponding Close value in the same row.
+    Sets missing values in Volume_(BTC) and Volume_(Currency) to 0.
+    Returns: the modified pd.DataFrame.
+
+
+    Args:
+        df (_type_): pd.DataFrame
+    """
+    df = df.drop(["Weighted_Price"], axis=1)
+    df['Close'] = df['Close'].ffill()  # forward fill default
+    df[['High', 'Low', 'Open']] = df[[
+        'High', 'Low', 'Open']].fillna(df['Close'])  # fill NaN
+    df['Volume_(BTC)', 'Volume_(Currency)'] = \
+        df['Volume_(BTC)', 'Volume_(Currency)'].fillna(0)
+    return df
