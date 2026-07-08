09f98cd9a5133d8a488c560ff9c2e2679f79094e
reha96 <reha.tuncer@gmail.com>
2026-07-08 08:45:27 +0200

    Create 5-slice.py

---
+++ pipeline/pandas/5-slice.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Write a function def slice(df): that takes a pd.DataFrame and:
+
+Extracts the columns High, Low, Close, and Volume_(BTC).
+Selects every 60th row from these columns.
+Returns: the sliced pd.DataFrame
+
+"""
+import pandas as pd
+
+
+def slice(df):
+    """takes a pd.DataFrame and ops
+
+    Args:
+        df (_type_): _description_
+    """
+    df = df[['High', 'Low', 'Close', 'Volume_(BTC)']]
+    df = df.loc[60]
+    return df
