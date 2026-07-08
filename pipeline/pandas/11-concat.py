209f10a5bd86d99ca2b850cd52260341487e4de2
reha96 <reha.tuncer@gmail.com>
2026-07-08 19:49:35 +0200

    Update 11-concat.py

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Write a function def concat(df1, df2):
-that takes two pd.DataFrame objects and:
+that takes two pd DataFrame objects and:
 
 Indexes both dataframes on their Timestamp columns.
 Includes all timestamps from df2 (bitstamp) up to
@@ -9,8 +9,7 @@ and including timestamp 1417411920.
 Concatenates the selected rows from df2 to the top of df1 (coinbase).
 Adds keys to the concatenated data, labeling the rows
 from df2 as bitstamp and the rows from df1 as coinbase.
-You should use index = __import__('10-index').index
-Returns the concatenated pd.DataFrame.
+Returns the concatenated pd DataFrame.
 
 """
 
