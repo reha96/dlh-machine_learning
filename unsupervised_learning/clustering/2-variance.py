ccc56c96892002f093b2c851c2f0123380c6f6f7
reha96 <reha.tuncer@gmail.com>
2026-07-30 08:59:43 +0200

    Update 2-variance.py

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -21,23 +21,6 @@ var is the total variance
         return None
     # re-compute clss with new C (if C changed)
     diffs = X[:, np.newaxis, :] - C
-    var = (diffs ** 2).sum(axis=2)
-    # clss = dists.argmin(axis=1)
+    # sum of the smallest squared distance for each points
+    var = (diffs ** 2).min(axis=1).sum()
     return var
-
-import numpy as np
-kmeans = __import__('1-kmeans').kmeans
-
-if __name__ == "__main__":
-    np.random.seed(0)
-    a = np.random.multivariate_normal([30, 40], [[16, 0], [0, 16]], size=50)
-    b = np.random.multivariate_normal([10, 25], [[16, 0], [0, 16]], size=50)
-    c = np.random.multivariate_normal([40, 20], [[16, 0], [0, 16]], size=50)
-    d = np.random.multivariate_normal([60, 30], [[16, 0], [0, 16]], size=50)
-    e = np.random.multivariate_normal([20, 70], [[16, 0], [0, 16]], size=50)
-    X = np.concatenate((a, b, c, d, e), axis=0)
-    np.random.shuffle(X)
-
-    for k in range(1, 11):
-        C, _ = kmeans(X, k)
-        print('Variance with {} clusters: {}'.format(k, variance(X, C).round(5)))
