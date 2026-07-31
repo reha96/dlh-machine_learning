f9e1f0effc48d451bc55a07b11aee2886b59d9b9
reha96 <reha.tuncer@gmail.com>
2026-07-31 09:49:57 +0200

    Create 5-pdf.py

---
+++ unsupervised_learning/clustering/5-pdf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Write a function that calculates the probability density function
+of a Gaussian distribution:
+    """
+import numpy as np
+
+
+def pdf(X, m, S):
+    """
+X is a numpy.ndarray of shape (n, d) containing the data points
+whose PDF should be evaluated
+
+m is a numpy.ndarray of shape (d,) containing the
+mean of the distribution
+
+S is a numpy.ndarray of shape (d, d) containing the
+covariance of the distribution
+
+You are not allowed to use any loops
+You are not allowed to use the function numpy.diag
+or the method numpy.ndarray.diagonal
+
+Returns: P, or None on failure
+
+P is a numpy.ndarray of shape (n,) containing the PDF values
+All values in P should have a minimum value of 1e-300
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    try:
+        m = X.mean(axis=0)
+        S = 
+    except Exception:
+        return None
