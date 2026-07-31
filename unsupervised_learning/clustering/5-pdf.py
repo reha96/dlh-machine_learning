44e5b2f6d53d6896d70d1b0bab03abb6bb6cc6fd
reha96 <reha.tuncer@gmail.com>
2026-07-31 10:27:46 +0200

    Update 5-pdf.py

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -30,9 +30,10 @@ All values in P should have a minimum value of 1e-300
     if not isinstance(m, np.ndarray) or len(m.shape) != 1:
         return None
     if not isinstance(S, np.ndarray) or len(S.shape) != 2 or \
-        S.shape[0] != S.shape[1]:
+            S.shape[0] != S.shape[1]:
         return None
     try:
-         
+        d = X.shape[1]
+        
     except Exception:
         return None
