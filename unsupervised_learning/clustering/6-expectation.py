9111d23f8888f052892c8d9db4f327abccd517a0
reha96 <reha.tuncer@gmail.com>
2026-08-02 22:24:42 +0200

    Update 6-expectation.py

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -21,19 +21,34 @@ def expectation(X, pi, m, S):
         probabilities
         l is the total log likelihood
     """
-    
+
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-            return None, None
-    if not isinstance(m, np.ndarray) or len(m.shape) != 1:
+        return None, None
+    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
         return None, None
     if not isinstance(S, np.ndarray) or len(S.shape) != 3 or \
             S.shape[1] != S.shape[2]:
         return None, None
-    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
+    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1 or \
+            pi.sum(axis=0) != 1:
         return None, None
     try:
         pdf = __import__('5-pdf').pdf
-        
-        
+
+        # initialize
+        k = pi.shape[0]  # clusters
+        n = X.shape[0]  # data points
+        g = np.zeros((k, n))  # posterior starts with zeros (k, n)
+
+        # bayes's rule, loop over k clusters: post = prior x likelihood
+        for i in range(k):
+            g[i] = pi[i] * pdf(X[i], m[i], S[i])
+            denominator += g[i]  # incrementally add evidence
+
+        g = g/denominator  # post = prior x likelihood / evidence
+        l = np.log(g).sum()  # log likelihood
+
+        return g, l
+
     except Exception:
         return None, None
