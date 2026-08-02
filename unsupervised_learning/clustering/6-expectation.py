4133e42103d0b97527271c024856cd71e782bbfa
reha96 <reha.tuncer@gmail.com>
2026-08-02 21:38:32 +0200

    Update 6-expectation.py

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -6,7 +6,8 @@ import numpy as np
 
 
 def expectation(X, pi, m, S):
-    """    X is a numpy.ndarray of shape (n, d) containing the data set
+    """
+    X is a numpy.ndarray of shape (n, d) containing the data set
     pi is a numpy.ndarray of shape (k,) containing the priors
 
     m is a numpy.ndarray of shape (k, d) containing the centroid means
@@ -19,6 +20,28 @@ def expectation(X, pi, m, S):
         g is a numpy.ndarray of shape (k, n) containing the posterior
         probabilities
         l is the total log likelihood
-    You should use pdf = __import__('5-pdf').pdf
     """
+    
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+            return None
+    if not isinstance(m, np.ndarray) or len(m.shape) != 1:
+        return None
+    if not isinstance(S, np.ndarray) or len(S.shape) != 2 or \
+            S.shape[0] != S.shape[1]:
+        return None
+    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
+        return None
+    try:
+        pdf = __import__('5-pdf').pdf
+        
+        # d dimensional
+        d = X.shape[1]
 
+        # multivariate normal pdf formula
+        const = (2*np.pi)**(-d/2)*(np.linalg.det(S))**(-1/2)
+        # (X - m) has shape (n, d), inv(S) has shape (d, d), sum along d
+        mahl = -1/2*(((X-m) @ np.linalg.inv(S) * (X-m)).sum(axis=1))
+        P = np.maximum(const * np.exp(mahl), 10**(-300))
+        return P
+    except Exception:
+        return None
