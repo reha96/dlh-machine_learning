b1a9ec345425b1caf4897dfd44b2c5a272921e67
reha96 <reha.tuncer@gmail.com>
2026-08-02 21:49:33 +0200

    Update 6-expectation.py

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -23,25 +23,17 @@ def expectation(X, pi, m, S):
     """
     
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-            return None
+            return None, None
     if not isinstance(m, np.ndarray) or len(m.shape) != 1:
-        return None
-    if not isinstance(S, np.ndarray) or len(S.shape) != 2 or \
-            S.shape[0] != S.shape[1]:
-        return None
+        return None, None
+    if not isinstance(S, np.ndarray) or len(S.shape) != 3 or \
+            S.shape[1] != S.shape[2]:
+        return None, None
     if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
-        return None
+        return None, None
     try:
         pdf = __import__('5-pdf').pdf
         
-        # d dimensional
-        d = X.shape[1]
-
-        # multivariate normal pdf formula
-        const = (2*np.pi)**(-d/2)*(np.linalg.det(S))**(-1/2)
-        # (X - m) has shape (n, d), inv(S) has shape (d, d), sum along d
-        mahl = -1/2*(((X-m) @ np.linalg.inv(S) * (X-m)).sum(axis=1))
-        P = np.maximum(const * np.exp(mahl), 10**(-300))
-        return P
+        
     except Exception:
-        return None
+        return None, None
