77f9364c11f93b2dac19fb9e066cbb4cdc7baa46
reha96 <reha.tuncer@gmail.com>
2026-08-04 10:08:00 +0200

    Update 7-maximization.py

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -25,8 +25,6 @@ S is a numpy.ndarray of shape (k, d, d) containing the updated covs
         return None, None, None
     if np.any(g < 0):  # posteriors are non-negative
         return None, None, None
-    if not np.all(np.isclose(g.sum(axis=0), 1)):  # each column sums to 1
-        return None, None, None
     try:
         n, d = X.shape  # data points, dims
         k = g.shape[0]  # clusters
