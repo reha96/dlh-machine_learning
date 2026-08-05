b21611a1d35ea9935b69eca977b4108df9f6b6fe
reha96 <reha.tuncer@gmail.com>
2026-08-05 09:32:52 +0200

    Update 10-kmeans.py

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -18,4 +18,7 @@ C is a numpy.ndarray of shape (k, d) containing the centroid means
 clss is a numpy.ndarray of shape (n,) containing the index of the
 cluster in C that each data point belongs to
     """
-    
+    kmeans = sklearn.cluster.KMeans(k).fit(X)
+    C = kmeans.cluster_centers_
+    clss = kmeans.labels_
+    return C, clss
