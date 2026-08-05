89212b08e8749526c272698adb644e080176df74
reha96 <reha.tuncer@gmail.com>
2026-08-05 08:46:37 +0200

    Update 9-BIC.py

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -2,6 +2,10 @@
 """Write a function that finds the best number of clusters for a GMM
 using the Bayesian Information Criterion:
     """
+
+import numpy as np
+
+
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """
 X is a numpy.ndarray of shape (n, d) containing the data set
@@ -21,14 +25,39 @@ best_result is tuple containing pi, m, S
 pi is a numpy.ndarray of shape (k,) containing the cluster priors
 m is a numpy.ndarray of shape (k, d) containing the centroid means
 S is a numpy.ndarray of shape (k, d, d) containing the covariance
-l is a numpy.ndarray of shape (kmax - kmin + 1) containing the ll
+ll is a numpy.ndarray of shape (kmax - kmin + 1) containing the ll
 b is a numpy.ndarray of shape (kmax - kmin + 1) containing the BIC
-Use: BIC = p * ln(n) - 2 * l
+Use: BIC = p * ln(n) - 2 * ll
 p is the number of parameters required for the model
 n is the number of data points used to create the model
 ll is the log likelihood of the model
-    
+
     """
-    
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None, None
+    if kmax is not None:
+        kmax = X.shape[0]
+    elif not isinstance(kmax, int) or kmax <= 0:
+        return None, None, None, None
+    if not isinstance(kmin, int) or kmin <= 0:
+        return None, None, None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None, None, None
+    if not isinstance(tol, (int, float)) or tol < 0:
+        return None, None, None, None
+    if not isinstance(verbose, bool):
+        return None, None, None, None
     expectation_maximization = __import__('8-EM').expectation_maximization
-    
+    try:
+        best_k = 0
+        best_result = (pi, m, S)
+        b = 0
+        n, d = X.shape
+        k = 0
+        for i in range(iterations):
+            pi, m, S, g, ll = expectation_maximization(X, k)
+            p = (k−1) + k·d + k·d(d+1)/2
+            b = p * ln(n) - 2 * ll
+        return best_k, best_result, ll, b
+    except Exception:
+        return None, None, None, None
