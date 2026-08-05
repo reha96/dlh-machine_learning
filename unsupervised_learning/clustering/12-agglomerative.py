e0655af20159ce7b0f8588645dca71c97da76a15
reha96 <reha.tuncer@gmail.com>
2026-08-05 10:06:53 +0200

    Update 12-agglomerative.py

--- unsupervised_learning/clustering/12-agglomerative.py
+++ unsupervised_learning/clustering/12-agglomerative.py
@@ -4,6 +4,8 @@
 
 import scipy.cluster.hierarchy
 import matplotlib.pyplot as plt
+
+
 def agglomerative(X, dist):
     """    
     X is a numpy.ndarray of shape (n, d) containing the dataset
@@ -14,5 +16,12 @@ def agglomerative(X, dist):
     Returns: clss, a numpy.ndarray of shape (n,) containing the cluster
     indices for each data point
     """
-    
-    
+    # Perform hierarchical/agglomerative clustering, Z is linkage mat
+    Z = scipy.cluster.hierarchy.linkage(X, method='ward')
+    # Form flat clusters from the hierarchical clustering defined by Z
+    clss = scipy.cluster.hierarchy.fcluster(Z, dist, criterion='distance')
+    # plot
+    scipy.cluster.hierarchy.dendrogram(
+        Z, color_threshold=dist, above_threshold_color='grey')
+    plt.show()
+    return clss
