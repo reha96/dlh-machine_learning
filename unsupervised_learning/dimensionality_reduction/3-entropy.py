d99f940c8eb975205e405078c874725a883112db
reha96 <reha.tuncer@gmail.com>
2026-08-05 20:46:53 +0200

    Update 3-entropy.py

--- unsupervised_learning/dimensionality_reduction/3-entropy.py
+++ unsupervised_learning/dimensionality_reduction/3-entropy.py
@@ -19,7 +19,8 @@ Pi, a numpy.ndarray of shape (n - 1,) containing the P affinities
     num = np.exp(-Di * beta)
     # step 2: normalize the weights into probabilities
     Pi = num / np.sum(num)
-    # step 3: Shannon entropy with log base 2
-    Hi = -np.sum(Pi * np.log2(Pi))
+    # step 3: Shannon entropy with log base 2, in a numerically stable form
+    # that never takes log2 of a zero: Hi = log2(sum) + beta * E[Di] / ln(2)
+    Hi = np.log2(np.sum(num)) + beta * np.sum(Di * Pi) / np.log(2)
 
     return Hi, Pi
