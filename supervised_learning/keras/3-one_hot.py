28d0c1536b6b317a5e50fc192ad373606942fbb7
reha96 <reha.tuncer@gmail.com>
2026-08-12 09:10:49 +0200

    Create 3-one_hot.py

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Converts a label vector into a one-hot matrix."""
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    """
+    Converts a label vector into a one-hot matrix.
+
+    Args:
+        labels: the labels to convert
+        classes (int): the number of classes
+
+    Returns:
+        the one-hot matrix
+    """
+    pass
