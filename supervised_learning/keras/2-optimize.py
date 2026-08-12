66319df03b702162ff378c9859dccb3bba32a8ec
reha96 <reha.tuncer@gmail.com>
2026-08-12 08:58:37 +0200

    Update 2-optimize.py

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -26,4 +26,6 @@ def optimize_model(network, alpha, beta1, beta2):
             beta_1=beta1,
             beta_2=beta2),
         # select loss function
-        loss='categorical_crossentropy')
+        loss='categorical_crossentropy',
+        # add accuracy
+        metrics=['accuracy'])
