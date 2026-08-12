7e572c7fea507a8971cabc67d04ccb1e08074284
reha96 <reha.tuncer@gmail.com>
2026-08-12 10:03:54 +0200

    Update 5-train.py

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -27,4 +27,6 @@ def train_model(network, data, labels, batch_size, epochs,
     Returns:
         the History object generated after training the model
     """
-    pass
+    return network.fit(x=data, y=labels, batch_size=batch_size,
+                       epochs=epochs, verbose=verbose, shuffle=shuffle,
+                       validation_data=validation_data)
