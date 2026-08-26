71ed96ba52b48840fc51a6bd1a3f69f840fdb8bb
reha96 <reha.tuncer@gmail.com>
2026-08-26 15:57:20 +0200

    Create 6-momentum.py

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Write a function def create_momentum_op(alpha, beta1): that sets up the
+gradient descent with momentum optimization algorithm in TensorFlow
+"""
+
+import tensorflow as tf
+
+
+def create_momentum_op(alpha, beta1):
+    """
+    Sets up the gradient descent with momentum optimization algorithm in
+    TensorFlow
+
+    alpha is the learning rate
+    beta1 is the momentum weight
+
+    Returns: optimizer
+    """
+    pass
