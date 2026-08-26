022669dbcdae24a28a9ff76e658f7fe879b1f1ef
reha96 <reha.tuncer@gmail.com>
2026-08-26 11:27:28 +0200

    Create 4-moving_average.py

---
+++ supervised_learning/optimization/4-moving_average.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Write a function def moving_average(data, beta): that calculates the
+weighted moving average of a data set
+"""
+
+
+def moving_average(data, beta):
+    """
+    Calculates the weighted moving average of a data set
+
+    data is the list of data to calculate the moving average of
+    beta is the weight used for the moving average
+
+    Your moving average calculation should use bias correction
+
+    Returns: a list containing the moving averages of data
+    """
+    pass
