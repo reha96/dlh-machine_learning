f2e893f7bba8a9f1eabe03de83a6a5be818e2257
reha96 <reha.tuncer@gmail.com>
2026-08-26 09:33:07 +0200

    Update 1-normalize.py

--- supervised_learning/optimization/1-normalize.py
+++ supervised_learning/optimization/1-normalize.py
@@ -22,21 +22,4 @@ def normalize(X, m, s):
 
     Returns: The normalized X matrix
     """
-    mu = np.mean(X, axis=0) 
-    sigma = np.std(X, axis=0)
-    
-
-
-if __name__ == '__main__':
-    np.random.seed(0)
-    a = np.random.normal(0, 2, size=(100, 1))
-    b = np.random.normal(2, 1, size=(100, 1))
-    c = np.random.normal(-3, 10, size=(100, 1))
-    X = np.concatenate((a, b, c), axis=1)
-    m, s = normalization_constants(X)
-    print(X[:10])
-    X = normalize(X, m, s)
-    print(X[:10])
-    m, s = normalization_constants(X)
-    print(m)
-    print(s)
+    return (X - m)/s
