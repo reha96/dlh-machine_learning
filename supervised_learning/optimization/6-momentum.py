bc74d34681d007c333faf6a7db6b63c752973872
reha96 <reha.tuncer@gmail.com>
2026-08-27 14:24:57 +0200

    Update 6-momentum.py

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -17,4 +17,5 @@ def create_momentum_op(alpha, beta1):
 
     Returns: optimizer
     """
-    pass
+    # all we did previously, with TensorFlow
+    return tf.keras.optimizers.SGD(learning_rate=alpha, momentum=beta1)
