f916bc3bf6ea6b95a54775e0e28eee2ac8f0d24d
reha96 <reha.tuncer@gmail.com>
2026-08-27 19:14:09 +0200

    Implement inverse time decay learning rate schedule

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -21,4 +21,10 @@ def learning_rate_decay(alpha, decay_rate, decay_step):
 
     Returns: the learning rate decay operation
     """
-    pass
+    return tf.keras.optimizers.schedules.InverseTimeDecay(
+        initial_learning_rate=alpha,
+        decay_steps=decay_step,
+        decay_rate=decay_rate,
+        staircase=False,
+        name='InverseTimeDecay'
+    )
