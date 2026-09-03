1a8bf8ee9975c8aec0852fde8e66722921da7cff
reha96 <reha.tuncer@gmail.com>
2026-09-03 13:55:24 +0200

    Implement F1 score calculation in f1_score function

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -18,4 +18,21 @@ def f1_score(confusion):
         numpy.ndarray of shape (classes,) containing the F1 score
         of each class.
     """
-    pass
+    # get all previous calculations
+    true_p = np.diag(confusion)
+
+    # sum of non diag elements ROW-WISE (across wrong predictions) equal FN
+    row_sum = np.sum(confusion, axis=1)
+    false_n = row_sum - true_p
+
+    # sum of non diag elements COL-WISE (across true labels) equal FP
+    col_sum = np.sum(confusion, axis=0)
+    false_p = col_sum - true_p
+
+    # according to formula
+    true_n = np.sum(confusion) - true_p - false_p - false_n
+
+    # f1 is 2 * (precision * recall) / (precision + recall)
+    # simplifies to TP / (TP + (FN+FP)/2)
+    f1 = true_p / (true_p + (false_n+false_p)/2)
+    return f1
