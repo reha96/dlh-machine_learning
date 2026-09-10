ad9e0c839eb56ac7b0c06eb42a1848ca99fc5481
reha96 <reha.tuncer@gmail.com>
2026-09-10 13:34:26 +0200

    Implement availableShips function to fetch starships based on passenger capacity

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -5,4 +5,20 @@ import requests
 
 def availableShips(passengerCount):
     """Return the list of ships that can hold a given number of passengers."""
-    pass
+    api_url = "https://swapi-api.hbtn.io/api/starships/"
+
+    out = []
+
+    r = requests.get(api_url)
+    r.raise_for_status()
+
+    while api_url:
+        r = requests.get(api_url)
+        r.raise_for_status()
+        response_json = r.json
+
+        # build the next API endpoint
+        count += 1
+        api_url = base_url.rstrip('/') + f'/api/quotes?page={count}'
+
+    return out
