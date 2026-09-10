ee5861bfd0d16749ff6c3cfbad8468cfd8348fe9
reha96 <reha.tuncer@gmail.com>
2026-09-10 14:09:41 +0200

    Refactor availableShips function to improve JSON handling and maintainability

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,24 +1,48 @@
 #!/usr/bin/env python3
 """Return the SWAPI starships able to carry a given number of passengers."""
 import requests
+import json
 
 
 def availableShips(passengerCount):
     """Return the list of ships that can hold a given number of passengers."""
-    api_url = "https://swapi-api.hbtn.io/api/starships/"
+
+    # create page counter
+    count = 1
+    # API url without page count
+    base = "https://swapi-api.hbtn.io/api/starships/"
+    # API endpoint, remove trailing backslash
+    api_url = base.rstrip('/') + f"/?page={count}"
 
     out = []
-
-    r = requests.get(api_url)
-    r.raise_for_status()
-
     while api_url:
+        # fetch JSON
         r = requests.get(api_url)
         r.raise_for_status()
-        response_json = r.json
+        response_json = r.text
 
-        # build the next API endpoint
-        count += 1
-        api_url = base_url.rstrip('/') + f'/api/quotes?page={count}'
+        # convert JSON to Python object
+        payload = json.loads(response_json)
+
+        # extract data
+        for ship in payload['results']:
+
+            # skip n/a
+            if ship['passengers'] in ('n/a', 'unknown'):
+                continue
+
+            # convert str to int
+            string = ship['passengers'].replace(",", "")
+            value = int(string)
+            if value >= passengerCount:
+                out.append(ship['name'])
+
+        # JSON null is Python None
+        if payload["next"] is None:
+            return out
+        else:
+            # build the next API endpoint
+            count += 1
+            api_url = base.rstrip('/') + f'/?page={count}'
 
     return out
