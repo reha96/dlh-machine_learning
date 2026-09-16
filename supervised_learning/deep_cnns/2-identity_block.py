4f1feba36ecce960e529f5bd0b0677d2198615e1
reha96 <reha.tuncer@gmail.com>
2026-09-16 13:45:15 +0200

    Implement identity block functionality in 2-identity_block.py

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -21,4 +21,24 @@ def identity_block(A_prev, filters):
     Returns:
         The activated output of the identity block.
     """
-    pass
+    # extract relevant dims
+    f11, f3, f12 = filters
+
+    # 3 blocks of 3 conv layers
+    # step 1: create conv layer with kernel 1x1
+    A_prev = K.layers.Conv2D(filters=f11, kernel_size=1, padding='same',
+                             kernel_initializer=K.initializers.HeNormal(seed=0))(A_prev)
+
+    # apply batch normalization and ReLu activation
+    # why axis 3? for c, channels axis of shape (m, h, w, c)
+    A_prev = K.layers.BatchNormalization(axis=3)(A_prev)
+    A_prev = K.layers.Activation('relu')(A_prev)
+
+    # step 2: create conv layer with kernel 3x3
+    A_prev = K.layers.Conv2D(filters=f3, kernel_size=3, padding='same',
+                             kernel_initializer=K.initializers.HeNormal(seed=0))(A_prev)
+
+    # apply batch normalization and ReLu activation
+    # why axis 3? for c, channels axis of shape (m, h, w, c)
+    A_prev = K.layers.BatchNormalization(axis=3)(A_prev)
+    A_prev = K.layers.Activation('relu')(A_prev)
