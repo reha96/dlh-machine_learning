1aacb4747422c0022aaaf9615af85a4c708213b7
reha96 <reha.tuncer@gmail.com>
2026-09-17 12:11:14 +0200

    Refactor build_feature_extractor to include model freezing and pooling layer

--- deep_learning/transfer_learning/0-frozen_extractor.py
+++ deep_learning/transfer_learning/0-frozen_extractor.py
@@ -14,11 +14,19 @@ def build_feature_extractor():
     Returns:
         A Keras Model mapping input images to extracted features.
     """
-    # load from Keras
+    # load from TF/Keras
     input_shape = (224, 224, 3)
+    
     # include top param: fully-connected layer at the top of the network
     base_m = K.applications.MobileNetV2(
         weights="imagenet", include_top=False,
         input_shape=input_shape)
 
-    return base_m
+    # freeze base (not trainable)
+    base_m.trainable = False
+    
+    # add 2D avg pooling to its output
+    # GlobalAveragePooling2D  reutrns  a 1D output by default vs 3D
+    base_m.output = K.layers.GlobalAveragePooling2D()(base_m.output)
+    model = K.Model(base_m.input, base_m.output)
+    return model
