2a35fc0269203444170364ba76cce37b41b1d9c8
reha96 <reha.tuncer@gmail.com>
2026-09-18 12:15:02 +0200

    Add centroid initialization and visualization example to clustering script

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -35,3 +35,21 @@ def initialize(X, k):
             return centroids
         except Exception:
             return None
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    np.random.seed(0)
+    a = np.random.multivariate_normal([30, 40], [[16, 0], [0, 16]], size=50)
+    b = np.random.multivariate_normal([10, 25], [[16, 0], [0, 16]], size=50)
+    c = np.random.multivariate_normal([40, 20], [[16, 0], [0, 16]], size=50)
+    d = np.random.multivariate_normal([60, 30], [[16, 0], [0, 16]], size=50)
+    e = np.random.multivariate_normal([20, 70], [[16, 0], [0, 16]], size=50)
+    X = np.concatenate((a, b, c, d, e), axis=0)
+    np.random.shuffle(X)
+    plt.scatter(X[:, 0], X[:, 1], s=10)
+    plt.show()
+    print(initialize(X, 5))
